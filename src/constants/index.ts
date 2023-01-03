3b57a84ec161869895db94a5713d20b5ee1a0c75
namjiyun <namjiyun3275@gmail.com>
2023-01-03 18:32:14 +0900

    feat: #3 add social login

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,3 +1,3 @@
-export { default as rm } from './responseMessage';
-export { default as sc } from './statusCode';
+export { default as message } from './responseMessage';
+export { default as statusCode } from './statusCode';
 export { default as tokenType } from './tokenType';
