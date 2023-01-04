11fb1596b27caab55bb55ddaae7262e9f22199c1
jeonghoon <shb03323@naver.com>
2023-01-04 17:58:13 +0900

    feat: #15 add dailyMissionService little

--- src/service/missionService.ts
+++ src/service/missionService.ts
@@ -1,7 +1,47 @@
+import dayjs from 'dayjs';
+import { PrismaClient } from '@prisma/client';
+const prisma = new PrismaClient();
+import convertSnakeToCamel from '../modules/convertSnakeToCamel';
 
+const getDailyMission = async (userId: number, date: string) => {
+  const user = await prisma.user.findUnique({
+    where: {
+      id: userId,
+    },
+  });
+  if (!user) {
+    return null;
+  }
 
-const getDailyMission
+  const actionDate = dayjs(date);
+
+  const dailyMissions = await prisma.mission.findMany({
+    where: {
+      user_id: userId,
+      action_date: actionDate.format(),
+    },
+    include: {
+      action: true,
+    },
+  });
+
+  // const data = await Promise.all(
+  //   timeTravelList.map(async (timeTravel) => {
+  //     const result: GetTimeTravelDto = {
+  //       timeTravelId: timeTravel._id,
+  //       title: timeTravel.title,
+  //       year: timeTravel.year,
+  //       month: timeTravel.month,
+  //       day: timeTravel.day,
+  //       writtenDate: timeTravel.writtenDate,
+  //       image: timeTravel.image,
+  //     };
+
+  //     return result;
+  //   }),
+  // );
+};
 
 export default {
-  
+  getDailyMission,
 };
