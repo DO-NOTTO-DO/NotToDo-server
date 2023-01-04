16673219e9b52a861a68755d94422e3b4b4a7f14
namjiyun <namjiyun3275@gmail.com>
2023-01-04 14:31:57 +0900

    feat: #13 add get category logic

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 
 import authRouter from './authRouter';
+import environmentRouter from './environmentRouter';
 
 const router: Router = Router();
 
 router.use('/auth', authRouter);
+router.use('/environment', environmentRouter);
 
 export default router;
