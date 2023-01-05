1e52c5ca1c42c324b5bd99ab97b29aa1ef5afbf6
namjiyun <namjiyun3275@gmail.com>
2023-01-06 00:25:52 +0900

    feat: #25 add getStatNotTodo router

--- src/router/missionRouter.ts
+++ src/router/missionRouter.ts
@@ -8,5 +8,6 @@ const router: Router = Router();
 router.get('/month/:month', auth, missionController.getMissionCount);
 router.get('/daily/:date', auth, missionController.getDailyMission);
 router.get('/week/:startDate', auth, missionController.getWeeklyMissionCount);
+router.get('/stat/notTodo', auth, missionController.getStatNotTodo);
 
 export default router;
