8399c8e511b22da63cd9a377c174ffa516e8c124
hyesuuou <68391767+hyesuuou@users.noreply.github.com>
2023-01-06 16:27:46 +0900

    fix: fix merge conflict

--- src/router/missionRouter.ts
+++ src/router/missionRouter.ts
@@ -8,7 +8,6 @@ const router: Router = Router();
 router.get('/month/:month', auth, missionController.getMissionCount);
 router.get('/daily/:date', auth, missionController.getDailyMission);
 router.get('/week/:startDate', auth, missionController.getWeeklyMissionCount);
-router.get('/stat/notTodo', auth, missionController.getNotTodoStat);
 router.get('/recent', auth, missionController.getRecentMissions);
 router.get('/stat/notTodo', auth, missionController.getNotTodoStat);
 router.get('/stat/situation', auth, missionController.getSituationStat);
