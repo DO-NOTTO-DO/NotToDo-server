4643638376bd2246e588a7d96c2e180f5fb36be9
jeonghoon <shb03323@naver.com>
2023-01-07 17:45:49 +0900

    test: #44 test getDailyMission api

---
+++ src/test/mission.spec.ts
@@ -0,0 +1,60 @@
+import app from '../index';
+import dotenv from 'dotenv';
+import req from 'supertest';
+dotenv.config();
+
+/**
+ * 일일 낫투두 조회
+ * 200, 400, 401 케이스
+ */
+describe('GET /mission/daily/:date', () => {
+  // 일일 낫투두 조회 200 케이스
+  it('일일 낫투두 조회 - 성공', (done) => {
+    req(app)
+      .get('/api/mission/daily/2023-01-06')
+      .set('Content-Type', 'application/json')
+      .set({ Authorization: `${process.env.TEST_ACCESS_TOKEN}` })
+      .expect(200)
+      .expect('Content-Type', /json/)
+      .then((res) => {
+        done();
+      })
+      .catch((err) => {
+        console.error('######Error >>', err);
+        done(err);
+      });
+  });
+  // 일일 낫투두 조회 400 케이스
+  it('일일 낫투두 조회 - 잘못된 날짜 형식', (done) => {
+    req(app)
+      .get(`/api/mission/daily/2023-25`)
+      .set('Content-Type', 'application/json')
+      .set({ Authorization: `${process.env.TEST_ACCESS_TOKEN}` })
+      .expect(400)
+      .expect('Content-Type', /json/)
+      .then((res) => {
+        done();
+      })
+      .catch((err) => {
+        console.error('######Error >>', err);
+        done(err);
+      });
+  });
+  // 일일 낫투두 조회 401 케이스
+  it('일일 낫투두 조회 - 유효하지 않은 토큰', (done) => {
+    req(app)
+      .get('/api/mission/daily/2023-01-05')
+      .set('Content-Type', 'application/json')
+      .set({ Authorization: `Bearer process.env.TEST_TOKEN` })
+      .expect(401)
+      .expect('Content-Type', /json/)
+      .then((res) => {
+        done();
+      })
+      .catch((err) => {
+        console.error('######Error >>', err);
+        done(err);
+      });
+  });
+});
+
