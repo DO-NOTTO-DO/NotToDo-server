befdfb5bb6aab0539789a879a971c4b9664cbf12
jeonghoon <shb03323@naver.com>
2023-01-07 18:16:37 +0900

    test: #44 test getWeeklyMissionCount api

--- src/test/mission.spec.ts
+++ src/test/mission.spec.ts
@@ -58,3 +58,73 @@ describe('GET /mission/daily/:date', () => {
   });
 });
 
+/**
+ * 주간 낫투두 개수 조회
+ * 200, 400, 401 케이스
+ */
+describe('GET /mission/week/:startDate', () => {
+  // 주간 낫투두 조회 200 케이스
+  it('주간 낫투두 개수 조회 - 성공', (done) => {
+    req(app)
+      .get('/api/mission/week/2023-01-02')
+      .set('Content-Type', 'application/json')
+      .set({ Authorization: `${process.env.TEST_ACCESS_TOKEN}` })
+      .expect(200)
+      .expect('Content-Type', /json/)
+      .then((res) => {
+        done();
+      })
+      .catch((err) => {
+        console.error('######Error >>', err);
+        done(err);
+      });
+  });
+  // 주간 낫투두 조회 400 케이스
+  it('주간 낫투두 개수 조회 - 잘못된 날짜 형식', (done) => {
+    req(app)
+      .get(`/api/mission/week/2023-25`)
+      .set('Content-Type', 'application/json')
+      .set({ Authorization: `${process.env.TEST_ACCESS_TOKEN}` })
+      .expect(400)
+      .expect('Content-Type', /json/)
+      .then((res) => {
+        done();
+      })
+      .catch((err) => {
+        console.error('######Error >>', err);
+        done(err);
+      });
+  });
+  // 주간 낫투두 조회 400 케이스
+  it('주간 낫투두 개수 조회 - 월요일이 아님', (done) => {
+    req(app)
+      .get(`/api/mission/week/2023-01-03`)
+      .set('Content-Type', 'application/json')
+      .set({ Authorization: `${process.env.TEST_ACCESS_TOKEN}` })
+      .expect(400)
+      .expect('Content-Type', /json/)
+      .then((res) => {
+        done();
+      })
+      .catch((err) => {
+        console.error('######Error >>', err);
+        done(err);
+      });
+  });
+  // 주간 낫투두 조회 401 케이스
+  it('주간 낫투두 조회 - 유효하지 않은 토큰', (done) => {
+    req(app)
+      .get('/api/mission/week/2023-01-02')
+      .set('Content-Type', 'application/json')
+      .set({ Authorization: `Bearer process.env.TEST_TOKEN` })
+      .expect(401)
+      .expect('Content-Type', /json/)
+      .then((res) => {
+        done();
+      })
+      .catch((err) => {
+        console.error('######Error >>', err);
+        done(err);
+      });
+  });
+});
