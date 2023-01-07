f0da52ba91e0ff181ed1fdc8968969d2b71fcf05
jeonghoon <shb03323@naver.com>
2023-01-07 11:19:57 +0900

    feat: #44 add exception of invalid date format

--- src/controllers/bannerController.ts
+++ src/controllers/bannerController.ts
@@ -20,6 +20,10 @@ const getBanner = async (req: Request, res: Response) => {
     const data = await bannerService.getBanner(userId, date);
     return res.status(statusCode.OK).send(success(statusCode.OK, message.READ_BANNER_SUCCESS, data));
   } catch (error) {
+    if (error === 400) {
+      res.status(statusCode.BAD_REQUEST).send(fail(statusCode.BAD_REQUEST, message.INVALID_DATE_TYPE));
+      return;
+    }
     const errorMessage: string = slackMessage(req.method.toUpperCase(), req.originalUrl, error, req.body.user?.id);
     sendMessageToSlack(errorMessage);
     res.status(statusCode.INTERNAL_SERVER_ERROR).send(fail(statusCode.INTERNAL_SERVER_ERROR, message.INTERNAL_SERVER_ERROR));
